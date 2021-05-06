8720d16dc2a898500df4173827dc397b5c92a6f4
Marut <marut.dhungana@gmail.com>
2021-05-06 14:04:48 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test<int> intObj = new Test<int>();
+            
+                   
+
+            intObj.Add(1);
+            intObj.Add(2);
+            intObj.Add(3);
+            intObj.Add(4);
+            intObj.Add(5);
+          
+
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine(intObj[i]);
+            
+            }
+
+            Test<string> strObj = new Test<string>();
+
+            strObj.Add("Apple");
+            strObj.Add("Ball");
+            strObj.Add("Cat");
+            strObj.Add("Dog");
+            strObj.Add("Elephant");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(strObj[i]);
+
+            }
+            Console.ReadKey();
+        }
+
+      
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExample
+{
+    class Test<T>
+    {
+        T[] obj = new T[5];
+        int count = 0;
+
+        public void Add(T item)
+        {
+            if (count + 1<6)
+            {
+                obj[count] = item;
+            }
+            count++;
+
+        }
+
+        public T this[ int index]
+        {
+            get { return obj[index]; }
+            set { obj[index] = value; }
+        }
+      
+    }
+}
